49cfe83ff49f38575d95cbb61f1138816dd7a61e
kiarapoke <kiarapoke@yahoo.es>
2015-12-07 09:16:32 +0000

    Inicio
    Iniciando mi proyectito, por ahora carga música

---
+++ Rhythm/Assets/Scripts/AudioLoader.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEditor;
+
+public class AudioLoader : MonoBehaviour {
+
+    System.IO.DirectoryInfo MusicFolder;
+    WWW myClip;
+    String myPath;
+ 
+    void Start()
+    {
+        LoadFile();
+    }
+
+    void LoadFile()
+    {
+        myPath = "C:\\Users\\Public\\Music\\Sample Music";
+        #if UNITY_ANDROID 
+        myPath = "/mnt/sdcard/music"; //Para moviles
+        #endif
+        MusicFolder = new System.IO.DirectoryInfo(myPath);
+        String myPath2 = EditorUtility.OpenFilePanel("", myPath, "mp3");
+        if (myPath2 != "")
+        {
+            myClip = new WWW("file:///" + myPath2);
+            this.gameObject.GetComponent<AudioSource>().clip = myClip.GetAudioClip(false, false);
+        }
+    }
+    
+}
---
+++ Rhythm/Assets/Scripts/RhythmController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RhythmController : MonoBehaviour {
+
+    public GameObject audioManager;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        if (audioManager.gameObject.GetComponent<AudioSource>().clip != null && !audioManager.gameObject.GetComponent<AudioSource>().isPlaying && audioManager.gameObject.GetComponent<AudioSource>().clip.loadState == AudioDataLoadState.Loaded)
+        {
+            audioManager.gameObject.GetComponent<AudioSource>().Play();
+        }
+    }
+}
